27b9ef20ae28da7a2b7f23da63316908911bd08e
jaeho.choi <jaeho614a@gmail.com>
2025-05-29 23:34:38 +0900

    index.js USER_DATA 변수명 변경

--- jaeho/index.js
+++ jaeho/index.js
@@ -1,6 +1,6 @@
 function deleteUserData() {
-  const USER_DATA = "userdata";
-  sessionStorage.removeItem(USER_DATA);
+  const USER_DATA_KEY = "userdata";
+  sessionStorage.removeItem(USER_DATA_KEY);
 }
 
 deleteUserData();
